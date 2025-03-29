0ded32ba1a03384841d3efbe75d166ea373be5c9
Codecatcatie <golovlevakatie@gmail.com>
2025-03-29 12:58:20 +0100

    debugging the screen and adding pokemon

---
+++ pokemon-game/movement.py
@@ -0,0 +1,4 @@
+#the goal is to crete a code that when the user  types the up arrow the whole character moves one step up . 
+#first code an input that whe the character inputs  the letter corresponing to the directions on the compass the whole character moves. 
+# so N (capital or lowercase) would get the whole chacarter to move one step up. S would get it to move down. E would move it right and W oudl move it left.
+#first step:get the input done and then how it should move accoridng to the letter inputed. 
